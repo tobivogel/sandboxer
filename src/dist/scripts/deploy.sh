7bed91bead40ee5a9d6d127d0f9cd968145840bd
tobi vogel <tvogel@thoughtworks.com>
2015-05-28 20:36:40 +0200

    removing precise dist target identifier....as there should always only be one

--- src/dist/scripts/deploy.sh
+++ src/dist/scripts/deploy.sh
@@ -5,7 +5,7 @@ GO_PIPELINE_NAME=${GO_PIPELINE_NAME-local}
 
 echo "copy files for dist.zip"
 cp ./src/dist/config/dev.yml config-$SERVICE_NAME.yml
-cp ./build/libs/$SERVICE_NAME-fat.jar app-$SERVICE_NAME.jar
+cp ./build/libs/*-fat.jar app-$SERVICE_NAME.jar
 
 echo "create dist.zip"
 zip dist-$SERVICE_NAME.zip config-$SERVICE_NAME.yml app-$SERVICE_NAME.jar
