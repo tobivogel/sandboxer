6325728665a8eaf649930b025cf18fad76533ce2
tobi vogel <tvogel@thoughtworks.com>
2015-05-30 17:48:08 +0200

    using nohup on startup command to start service in background and let build phase pass

--- src/dist/scripts/run.sh
+++ src/dist/scripts/run.sh
@@ -5,4 +5,4 @@ DEPLOY_DIR=/prod/$SERVICE_NAME
 
 echo "run $SERVICE_NAME service"
 ssh -i /root/.ssh/id_rsa -o "StrictHostKeyChecking no" vagrant@12.12.12.12 "unzip /prod/dist-$SERVICE_NAME.zip -d $DEPLOY_DIR"
-ssh -i /root/.ssh/id_rsa -o "StrictHostKeyChecking no" vagrant@12.12.12.12 "java -jar $DEPLOY_DIR/app-$SERVICE_NAME.jar server $DEPLOY_DIR/config-$SERVICE_NAME.yml"
+ssh -i /root/.ssh/id_rsa -o "StrictHostKeyChecking no" vagrant@12.12.12.12 "nohup java -jar $DEPLOY_DIR/app-$SERVICE_NAME.jar server $DEPLOY_DIR/config-$SERVICE_NAME.yml &> /dev/null &"
