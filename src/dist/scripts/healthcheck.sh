83dd45686916cff40d4eca21b948d4d8dac3ba1b
tobi vogel <tvogel@thoughtworks.com>
2015-06-01 23:08:48 +0200

    adding healthcheck script

---
+++ src/dist/scripts/healthcheck.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+HOST=${HOST-localhost:8091}
+
+status="fail"
+max_checks=10
+
+set +e
+for i in `seq 1 $max_checks`; do
+  echo "${i}. try"
+
+  out=`curl -fsS --max-time 5 "http://$HOST/healthcheck"`
+
+  if [ $? -eq 0 ]; then
+    status="success"
+    echo $status
+    break
+  else
+    sleep 1
+  fi
+done
+set -e
+
+if [ "${status}" != "success" ]; then
+  echo "healthcheck failed"
+  exit 1
+fi
